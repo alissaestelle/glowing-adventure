ea122c679729568fe319dcc361c964de166f25d6
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-03-29 09:48:06 -0400

    Create Post Function Modified

--- controllers/controlPost.js
+++ controllers/controlPost.js
@@ -1,11 +1,26 @@
 const db = require('../db')
 const { Post } = require('../models')
 
+const consolePosts = async () => {
+  const posts = await Post.find()
+  console.log(posts)
+}
+
 const getPosts = async (req, res) => {
   const posts = await Post.find()
   res.json(posts)
 }
 
+const createPost = async (req, res) => {
+  const model = await Post.create(req.body, (err) => {
+    res.send(err)
+    console.log(model)
+  })
+  // res.json(model)
+  await consolePosts()
+}
+
 module.exports = {
-  getPosts
+  getPosts,
+  createPost
 }
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const logger = require('morgan')
-const PORT = process.env.PORT || 3001
 const db = require('./db')
+const controlPost = require('./controllers/controlPost')
+
+const PORT = process.env.PORT || 3001
 
 const app = express()
 
@@ -15,6 +17,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/posts', controlPost.getPosts)
+app.post('/posts', controlPost.createPost)
 
 app.listen(PORT, () => {
   console.log(`Express serving up blog bunnies on port ${PORT} 🐰`)
