46681d4a36bf47a07e2d0fdc66a7e31c09ffc422
Alissa Wiley <alissa.estelle.w@gmail.com>
2022-03-29 11:30:11 -0400

    Homepage Formatted to Gridbox

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,31 @@
+import Header from '../components/Header'
 import Search from '../components/Search'
+import Article from '../components/Article'
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <Search />
-      </div>
-      <div>
-        <input />
-      </div>
+    <div className="Home">
+      <Header />
+      <Search />
+      <aside>Testing out a side bar!</aside>
+      <main className="Posts">
+        <h1>Posts</h1>
+        {/* <Article /> */}
+        <article>
+          <header>
+            <h3>Post Title</h3>
+            <p>By: Author</p>
+          </header>
+          <p>This is content!</p>
+        </article>
+        <article>
+          <header>
+            <h3>Post Title</h3>
+            <p>By: Author</p>
+          </header>
+          <p>This is content!</p>
+        </article>
+      </main>
     </div>
   )
 }
